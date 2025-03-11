1b12a52dc87403caff215db2d38d6480f95b7bd4
Stephanie Adeoye <mojolaoluwaadeoye@gmail.com>
2025-03-11 14:56:51 -0400

    steph/feat/csv-upload/changed file location in local system

--- upload.js
+++
@@ -1,7 +0,0 @@
-//This is the JavaScript code that allows users to upload a CSV file
-document.getElementById("file-upload").addEventListener("change", function(event) {
-    const file = event.target.files[0];
-    if (file) {
-        document.getElementById("file-name").textContent = `Selected File: ${file.name}`;
-    }
-});
