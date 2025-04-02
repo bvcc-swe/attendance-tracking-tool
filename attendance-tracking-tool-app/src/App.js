afa835b1ea0b4327080cbb6c74c4d70b16cd7b62
Stephanie Adeoye <mojolaoluwaadeoye@gmail.com>
2025-04-02 15:01:35 -0400

    feat: Imported the button for uploading the csv files into the react app

--- attendance-tracking-tool-app/src/App.js
+++ attendance-tracking-tool-app/src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import CsvUpload from './components/CsvUpload';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <CsvUpload />
         <a
           className="App-link"
           href="https://reactjs.org"
