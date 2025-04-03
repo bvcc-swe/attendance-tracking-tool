0910428583f2581fab7eaa5ae9e52356137a8628
Stephanie Adeoye <mojolaoluwaadeoye@gmail.com>
2025-04-03 06:40:33 -0400

    feat: imported student profile page and student profile button

--- attendance-tracking-tool-app/src/App.js
+++ attendance-tracking-tool-app/src/App.js
@@ -1,12 +1,22 @@
-import logo from './logo.svg';
+import React from "react";
 import './App.css';
-import UploadButton from '/.components/UploadButtonComponent';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import StudentProfilePage from "./components/StudentProfilePage.js";
+import StudentProfileButton from "./components/StudentProfileButton";
 
 function App() {
   return (
-    <div className="App">
-        <UploadButton/>
-    </div>
+    <Router>
+      <div>
+        {/* the button used to navigate to the student profile page is rendered here */}
+        <StudentProfileButton />
+
+        <Routes>
+          {/* Define the route for the Student Profile page */}
+          <Route path="/student-profile" element={<StudentProfilePage />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
