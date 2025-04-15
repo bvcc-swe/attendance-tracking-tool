047bfae310469bbf5596894a66561ac6bf32c186
Stephanie Adeoye <mojolaoluwaadeoye@gmail.com>
2025-04-15 19:39:44 -0400

    feat: imported the CsvParser.js into the app.js

--- attendance-tracking-tool-app/src/App.js
+++ attendance-tracking-tool-app/src/App.js
@@ -1,13 +1,15 @@
 import React from "react";
 import './App.css';
-import StudentProfile from "./components/StudentProfile.js";
+//import UploadButton from "./components/UploadButtonComponent"; // Adjust path if needed
+import CsvParser from "./components/CsvParser"; // Adjust path if needed
 
 function App() {
   return (
     <div className="App">
        {/* Render the StudentProfile page */}
-       <StudentProfile />
+       <CsvParser />
     </div>
+    // Did not render the upload button since it is being used in the Csv Parser
   );
 }
 
