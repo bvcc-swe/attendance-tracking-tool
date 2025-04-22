54a9ef489d12709a8bbec0a706a97e65b176d6ac
Stephanie Adeoye <mojolaoluwaadeoye@gmail.com>
2025-04-22 02:05:01 -0400

    fix: show ‘View Student Profiles’ button only on homepage, hide on profile view page”

--- attendance-tracking-tool-app/src/App.js
+++ attendance-tracking-tool-app/src/App.js
@@ -1,23 +1,35 @@
 import './App.css';
 import UploadButton from './components/UploadButtonComponent';
 import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes, useLocation } from "react-router-dom";
 import StudentProfilePage from "./components/StudentProfilePage.js";
+import StudentProfileButton from './components/StudentProfileButton';
 
+
+function AppContent() {
+  const location = useLocation();
+
+  const isOnStudentProfilePage = location.pathname === "/student-profile";
+
+  return (
+    <div>
+      {/* Always show UploadButton */}
+      <UploadButton />
+
+      {/* Only show StudentProfileButton if not on the profile page */}
+      {!isOnStudentProfilePage && <StudentProfileButton />}
+
+      <Routes>
+        <Route path="/student-profile" element={<StudentProfilePage />} />
+      </Routes>
+    </div>
+  );
+}
 
 function App() {
   return (
     <Router>
-      <div>
-        {/* the button used to upload the CSV is rendered here */}
-        <UploadButton />
-        
-
-        <Routes>
-          {/* Define the route for the Student Profile page */}
-          <Route path="/student-profile" element={<StudentProfilePage />} />
-        </Routes>
-      </div>
+      <AppContent />
     </Router>
   );
 }
