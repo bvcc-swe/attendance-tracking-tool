cf797e8e48e77b56d5b626a683f8f4cf4941d9e7
Stephanie Adeoye <mojolaoluwaadeoye@gmail.com>
2025-04-22 01:46:24 -0400

    fix: removed "view student profile" button from student profile page

--- attendance-tracking-tool-app/src/App.js
+++ attendance-tracking-tool-app/src/App.js
@@ -3,15 +3,15 @@ import UploadButton from './components/UploadButtonComponent';
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import StudentProfilePage from "./components/StudentProfilePage.js";
-import StudentProfileButton from "./components/StudentProfileButton";
+
 
 function App() {
   return (
     <Router>
       <div>
+        {/* the button used to upload the CSV is rendered here */}
         <UploadButton />
-        {/* the button used to navigate to the student profile page is rendered here */}
-        <StudentProfileButton />
+        
 
         <Routes>
           {/* Define the route for the Student Profile page */}
