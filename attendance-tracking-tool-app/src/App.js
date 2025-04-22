65c5427499666935ba239638a075f6870c855a9d
Stephanie Adeoye <mojolaoluwaadeoye@gmail.com>
2025-04-22 10:41:22 -0400

    Updated routes for buttons

--- attendance-tracking-tool-app/src/App.js
+++ attendance-tracking-tool-app/src/App.js
@@ -8,16 +8,21 @@ import StudentProfileButton from "./components/StudentProfileButton";
 function App() {
   return (
     <Router>
-      <div>
-        <UploadButton />
-        {/* the button used to navigate to the student profile page is rendered here */}
-        <StudentProfileButton />
+      <Routes>
+        {/* Homepage route */}
+        <Route
+          path="/"
+          element={
+            <div>
+              <UploadButton />
+              <StudentProfileButton />
+            </div>
+          }
+        />
 
-        <Routes>
-          {/* Define the route for the Student Profile page */}
-          <Route path="/student-profile" element={<StudentProfilePage />} />
-        </Routes>
-      </div>
+        {/* Student profile page route */}
+        <Route path="/student-profile" element={<StudentProfilePage />} />
+      </Routes>
     </Router>
   );
 }
